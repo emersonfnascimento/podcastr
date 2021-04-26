643c635a9b2f7edb2f947b0da71a1b66a17b8823
Emerson Freitas <emersonfreitasn@gmail.com>
2021-04-26 11:29:41 -0500

    exibindo informações e estilizações do player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,29 @@ import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 
 import styles from '../styles/app.module.scss'
+import { PlayerContext } from '../context/PlayerContext'
+import { useState } from 'react'
+import { Head } from 'next/document'
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play}}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
